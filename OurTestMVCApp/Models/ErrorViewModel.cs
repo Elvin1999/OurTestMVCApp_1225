4b36daf625584843402c96cd95ea8a0a5a5b789f
camalzadeelvin <camalzadeelvin@STHQ0118-01>
2024-05-18 09:40:39 +0400

    Add project files.

---
+++ OurTestMVCApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OurTestMVCApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
